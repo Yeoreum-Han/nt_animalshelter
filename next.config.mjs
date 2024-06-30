5a3e063636f12dc11465ed49a4792e0d52c698d0
Yeoreum Han <yrh.summer@gmail.com>
2024-06-30 19:17:38 +0900

    rewrites_api

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,14 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: '/:aniaml/:cities/selectResult',
-                destination: `${process.env.NEXT_PUBLIC_DATA_URL}?numOfRows=223&pageNo=1&_type=json&serviceKey=${process.env.NEXT_PUBLIC_API_ENC_KEY}`,
+                source: '/:aniaml/:cities/selectResult/api',
+                destination: '/:aniaml/:cities/selectResult',
             },
+            {
+                source: '/searchResult/api',
+                destination: '/searchResult',
+            },
+
         ]
     },
 };
---
+++ src/app/(select)/[animal]/[cities]/selectResult/api/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server"
+   
+  export async function GET() {
+
+    const API_URL = process.env.NEXT_PUBLIC_DATA_URL
+    const ENC_KEY = process.env.NEXT_PUBLIC_API_ENC_KEY
+    // 전체 데이터 개수 data.response.body.items.item.totalCount값
+    const totalData = 223
+    const res = await fetch(`${API_URL}?numOfRows=${totalData}&pageNo=1&_type=json&serviceKey=${ENC_KEY}`)
+    const data = await res.json()
+
+    return NextResponse.json({data})
+  }
